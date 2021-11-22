177fd221118f46c4c4e65a5ed10d1177fbf696b4
heyue <2908839238@qq.com>
2021-11-22 16:29:29 +0800

    init

---
+++ App.vue
@@ -0,0 +1,173 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	
+	/*每个页面公共css */
+	/**app.wxss**/
+	
+	page{
+	  background-color: #e3dae9;
+	}
+	@font-face {
+	  font-family: 'iconfont';  /* Project id 2895082 */
+	  src: url('//at.alicdn.com/t/font_2895082_djj4pt421jv.woff2?t=1636076950756') format('woff2'),
+	       url('//at.alicdn.com/t/font_2895082_djj4pt421jv.woff?t=1636076950756') format('woff'),
+	       url('//at.alicdn.com/t/font_2895082_djj4pt421jv.ttf?t=1636076950756') format('truetype');
+	}
+	
+	.iconfont {
+	  font-family: "iconfont" !important;
+	  font-size: 16px;
+	  font-style: normal;
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	}
+	
+	.icon-yangqitingshu:before {
+	  content: "\e62f";
+	}
+	
+	.icon-fenlei:before {
+	  content: "\e60a";
+	}
+	
+	.icon-paihangbang:before {
+	  content: "\e65a";
+	}
+	
+	.icon-zanting1:before {
+	  content: "\e87a";
+	}
+	
+	.icon-bofang:before {
+	  content: "\e87c";
+	}
+	
+	.icon-renqi1:before {
+	  content: "\e601";
+	}
+	
+	.icon-renqizhi:before {
+	  content: "\e61d";
+	}
+	
+	.icon-renqi2:before {
+	  content: "\e943";
+	}
+	
+	.icon-jiangpai-copy:before {
+	  content: "\e600";
+	}
+	
+	.icon-dier:before {
+	  content: "\e637";
+	}
+	
+	.icon-disan:before {
+	  content: "\e633";
+	}
+	
+	.icon-diyi:before {
+	  content: "\e6e9";
+	}
+	
+	.icon-daochu1024-26:before {
+	  content: "\e617";
+	}
+	
+	.icon-daochu1024-27:before {
+	  content: "\e618";
+	}
+	
+	.icon-daochu1024-28:before {
+	  content: "\e619";
+	}
+	
+	.icon-huoyan:before {
+	  content: "\e63e";
+	}
+	
+	.icon-renqi:before {
+	  content: "\e609";
+	}
+	
+	.icon-fl-renyuan:before {
+	  content: "\e625";
+	}
+	
+	.icon-yinpai:before {
+	  content: "\e606";
+	}
+	
+	.icon-jinpai:before {
+	  content: "\e608";
+	}
+	
+	.icon-tongpai:before {
+	  content: "\e616";
+	}
+	
+	.icon-15:before {
+	  content: "\e61e";
+	}
+	
+	.icon-7:before {
+	  content: "\e638";
+	}
+	
+	.icon-changyong_fanhui:before {
+	  content: "\e6aa";
+	}
+	
+	.icon-changyong_xiala:before {
+	  content: "\e6ae";
+	}
+	
+	.icon-changyong_bofang:before {
+	  content: "\e6af";
+	}
+	
+	.icon-changyong_chenggong:before {
+	  content: "\e6b0";
+	}
+	
+	.icon-changyong_dianzan:before {
+	  content: "\e6b1";
+	}
+	
+	.icon-changyong_fangda:before {
+	  content: "\e6b2";
+	}
+	
+	.icon-changyong_shezhi:before {
+	  content: "\e6b3";
+	}
+	
+	.icon-changyong_xihuan:before {
+	  content: "\e6b5";
+	}
+	
+	.icon-shinei_lvzhi02:before {
+	  content: "\e6d0";
+	}
+	
+	.icon-shinei_yangtai02:before {
+	  content: "\e6d1";
+	}
+	
+	.icon-shinei_fengshan02:before {
+	  content: "\e6d2";
+	}
+</style>
---
+++ components/store/store.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<view class="shortp">
+		    <view class="title">
+		        <view class="titleTag">
+		            <text class="iconfont icon-jinpai"></text>
+		            <slot name="title">
+		        </view>
+		        <view class="more">
+		            <navigator url="/pages/list/list?id=5" hover-class="none">
+		                更多
+		                <text class="iconfont icon-changyong_fanhui"></text>
+		            </navigator>
+		        </view>
+		    </view>
+		    <view class="shorpcontent">
+		        <view class="shortpitem" wx:for="{{shortps}}" wx:for-item="item" wx:key="id">
+		            <navigator url="/pages/bookinfo/bookinfo?id={{item.BookId}}" hover-class="none">
+		                <view class="itemimg">
+		                    <image src="{{item.Cover}}" mode="widthFix"></image>
+		                </view>
+		                <view class="">{{item.Name}}</view>
+		                <view class="author">{{item.Author}}</view>
+		            </navigator>
+		        </view>
+		    </view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"store",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
